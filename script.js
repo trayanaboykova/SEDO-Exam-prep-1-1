390eacb7797bc6573711b4bed8485da80807435b
Trayana Boykova <101351760+trayanaboykova@users.noreply.github.com>
2025-06-27 11:59:45 +0300

    commit 1 (#6)

--- script.js
+++ script.js
@@ -24,7 +24,4 @@ function calculateAndDisplay(fn) {
   document.getElementById('result').textContent = fn(a, b);
 }
 
-['add', 'subtract', 'multiply'].forEach(op => {
-  document.getElementById(`btn-${op}`)
-    .addEventListener('click', () => calculateAndDisplay(window[op]));
-});
+
